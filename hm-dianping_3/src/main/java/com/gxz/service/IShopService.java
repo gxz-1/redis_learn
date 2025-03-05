a2c86c03cb6c9eea0ec9ff6144b793365f5ff2ca
gao_lenovo <xizhuogao@foxmail.com>
2025-03-05 10:53:28 +0800

    finish CacheClient

--- hm-dianping_3/src/main/java/com/gxz/service/IShopService.java
+++ hm-dianping_3/src/main/java/com/gxz/service/IShopService.java
@@ -1,5 +1,6 @@
 package com.gxz.service;
 
+import com.gxz.dto.Result;
 import com.gxz.entity.Shop;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IShopService extends IService<Shop> {
 
+    Result queryById(Long id);
 }
--- hm-dianping_3/src/main/java/com/gxz/service/impl/ShopServiceImpl.java
+++ hm-dianping_3/src/main/java/com/gxz/service/impl/ShopServiceImpl.java
@@ -1,10 +1,18 @@
 package com.gxz.service.impl;
 
+import com.gxz.dto.Result;
 import com.gxz.entity.Shop;
 import com.gxz.mapper.ShopMapper;
 import com.gxz.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gxz.utils.CacheClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.gxz.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.gxz.utils.RedisConstants.CACHE_SHOP_TTL;
 
 /**
  * <p>
@@ -16,5 +24,27 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Autowired
+    private CacheClient cacheClient;
 
+    @Override
+    public Result queryById(Long id) {
+        // 解决缓存穿透
+        Shop shop = cacheClient
+                .queryWithPassThrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        // 互斥锁解决缓存击穿
+        // Shop shop = cacheClient
+        //         .queryWithMutex(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+
+        // 逻辑过期解决缓存击穿
+        // Shop shop = cacheClient
+        //         .queryWithLogicalExpire(CACHE_SHOP_KEY, id, Shop.class, this::getById, 20L, TimeUnit.SECONDS);
+
+        if (shop == null) {
+            return Result.fail("店铺不存在！");
+        }
+        // 7.返回
+        return Result.ok(shop);
+    }
 }
