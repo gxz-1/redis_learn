ba85772941e81437b096a67a01295731351568c3
gao_lenovo <xizhuogao@foxmail.com>
2025-03-07 16:45:20 +0800

    redission start

---
+++ redission_4/src/main/java/com/gxz/config/RedissonConfig.java
@@ -0,0 +1,21 @@
+package com.gxz.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient(){
+        // 配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://218.194.61.54:9872")
+            .setPassword("password");
+        // 创建RedissonClient对象
+        return Redisson.create(config);
+    }
+}
